e019806b5e4c12aff7684f26a3f72c6790593591
Adrian Hall <adrian@shellmonger.com>
2015-04-26 16:45:29 -0700

    v0.0.2 View Engine

--- basic-webapp/index.js
+++ basic-webapp/index.js
@@ -1,11 +1,23 @@
 var express = require("express");
+var partials = require("express-partials");
+var ejs = require("ejs");
+
 var app = express();
+
+// Set Express to use the EJS view engine
+app.engine("html", ejs.renderFile);
+app.set("view engine", "html");
+
+// Set up express to use layouts
+app.use(partials());
 
 // Anything under /client is served as a static file
 app.use("/client", express.static("client"));
 
 app.get("/", function(req, res) {
-  res.redirect("/client/index.html");
+  res.render("index", {
+    title: "Index Page (from index.js)"
+  });
 });
 
 var server = app.listen(3000, function() {
